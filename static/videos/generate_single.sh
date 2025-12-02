0cbb488e2ed8e8e5fbf8ee15c3df8fac8b93f625
tuallen <allentu3@gmail.com>
2025-12-02 11:23:19 -0500

    Playroom single

--- static/videos/generate_single.sh
+++ static/videos/generate_single.sh
@@ -6,18 +6,18 @@
 #   [stack]drawbox=x=488:y=0:w=8:h=1080:color=white:t=fill[out]
 # " -map "[out]" -c:v libx264 -crf 16 -preset slow -pix_fmt yuv420p playroom_single.mp4
 
-ffmpeg -y -i playroom.mp4 -filter_complex "
-split=2[a][b];
-[a]crop=1920:1080:0:0[L];
-[b]crop=1920:1080:1920:0[R];
-[L][R]blend=all_expr='if(lte(X,W*(1-abs(mod(T/1.996,2)-1))),A,B)'[blend];
-[blend]geq=lum='if(between(X, W*(1-abs(mod(T/1.996,2)-1))-4, W*(1-abs(mod(T/1.996,2)-1))+4),255,lum(X,Y))':cb='cb(X,Y)':cr='cr(X,Y)'[out]
-" -map "[out]" -c:v libx264 -crf 16 -preset veryslow -pix_fmt yuv420p playroom_single.mp4
-
 # ffmpeg -y -i playroom.mp4 -filter_complex "
 # split=2[a][b];
 # [a]crop=1920:1080:0:0[L];
 # [b]crop=1920:1080:1920:0[R];
-# [L][R]blend=all_expr='if(lte(X,W*(1-abs(mod(T/1.660,2)-1))),A,B)'[blend];
-# [blend]geq=lum='if(between(X, W*(1-abs(mod(T/1.660,2)-1))-4, W*(1-abs(mod(T/1.660,2)-1))+4),255,lum(X,Y))':cb='cb(X,Y)':cr='cr(X,Y)'[out]
+# [L][R]blend=all_expr='if(lte(X,W/2*(1-abs(mod(T/1.996,2)-1))+W/2),A,B)'[blend];
+# [blend]geq=lum='if(between(X, W/2*(1-abs(mod(T/1.996,2)-1))+W/2-4, W/2*(1-abs(mod(T/1.996,2)-1))+W/2+4),255,lum(X,Y))':cb='cb(X,Y)':cr='cr(X,Y)'[out]
 # " -map "[out]" -c:v libx264 -crf 16 -preset veryslow -pix_fmt yuv420p playroom_single.mp4
+
+ffmpeg -y -i playroom.mp4 -filter_complex "
+split=2[a][b];
+[a]crop=1920:1080:0:0[L];
+[b]crop=1920:1080:1920:0[R];
+[L][R]blend=all_expr='if(lte(X,W/2+W/2*sin(2*PI*T/3.992)),A,B)'[blend];
+[blend]geq=lum='if(between(X, W/2+W/2*sin(2*PI*T/3.992)-4, W/2+W/2*sin(2*PI*T/3.992)+4),255,lum(X,Y))':cb='cb(X,Y)':cr='cr(X,Y)'[out]
+" -map "[out]" -c:v libx264 -crf 16 -preset veryslow -pix_fmt yuv420p playroom_single.mp4
